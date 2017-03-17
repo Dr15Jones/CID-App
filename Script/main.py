4c3cd16bacba3ad5a92a5949128c56f9a1abe8b7
Chris Jones <chrisdjones15@gmail.com>
2017-03-16 20:48:09 -0500

    Encapsulated data model
    Moved all data model related handling into the DataSource class and its composite classes.

--- Script/main.py
+++ Script/main.py
@@ -2,7 +2,7 @@ import ui
 import json
 
 #############################
-# Building Types
+# Data Model
 #############################
 class HitBoxQualities(object):
 	def __init__(self, active = False, label = None):
@@ -19,39 +19,190 @@ class ModelType (object):
 			self._specialBoxes = specialBoxes
 		else:
 			self._specialBoxes = {}
-gKnownTypes = [ModelType('New')]
+
+class ModelTypeProxy(object):
+	def __init__(self,changeManager, type):
+		self.__cm = changeManager
+		self.__type = type
+	@property
+	def name(self):
+		return self.__type._name
+	@name.setter
+	def name(self,newName):
+		self.__type._name = newName
+		self.__cm.store_changes()
+
+	def has_box(self,column,index):
+		return (column, index) in self.__type._specialBoxes
+	def add_box(self, column, index, active=False, label=None):
+		self.__type._specialBoxes[(column,index)] =HitBoxQualities(active,label)
+		self.__cm.store_changes()
+	def remove_box(self,column, index):
+		if (column,index) in self._specialBoxes:
+			del self.__type._specialBoxes[(column,index)]
+			self.__cm.store_changes()
+
+class Model (object):
+	def __init__(self,type,name,hits=None):
+		self._name = name
+		self._type = type
+		if hits:
+			self._hitBoxes =hits
+		else:
+			self._hitBoxes = {}
+
+
+class ModelProxy(object):
+	def __init__(self,changeManager, model):
+		self.__model = model
+		self.__cm = changeManager
+	@property
+	def name(self):
+		return self.__model._name
+	@name.setter
+	def name(self,newName):
+		self.__model._name = newName
+		self.__cm.store_changes()
+	@property
+	def type(self):
+		return self.__model._type
+	@type.setter
+	def type(self,t):
+		self.__model._type = t
+		self.__cm.store_changes()
+	def was_hit(self,column,index):
+		return (column,index) in self.__model._hitBoxes
+	def add_hit(self,column,index):
+		self.__model._hitBoxes[(column,index)] = True
+		self.__cm.store_changes()
+	def remove_hit(self,column,index):
+		if (column,index) in self.__model._hitBoxes:
+			del self.__model.hitBoxes[column,index]
+			self.__cm.store_changes()
+
+class DataSource(object):
+	"""
+		Contains the Model types and model instances in use
+	"""
+	def __init__(self):
+		self._types = [ModelType('New')]
+		self._models = []
+		self._fileName = ".cid"
+
+	def type(self,index):
+		return ModelTypeProxy(self,self._types[index])
+
+	def type_names(self):
+		return [ x._name for x in self._types]
+
+	def types_to_keep(self,typeNameList):
+		returnValue= False
+		d = { x._name:x for x in self._types}
+		self._types = []
+		newNames = set()
+		for n in typeNameList:
+			self._types.append(d[n])
+			newNames.add(n)
+		if len(self._types) < len(d):
+			returnValue = True
+			#the selected item was probably deleted
+			for n in d.keys():
+				if n not in newNames:
+					self.__remove_all_models_of_type(d[n])
+			self.store_changes()
+		return returnValue
+
+	def add_type(self,type):
+		self._types.append(type)
+		self.store_changes()
+
+	def __remove_all_models_of_type(self,type):
+		self._models = [ x for x in self._models if x._type != type]
+
+	def model(self,index):
+		return ModelProxy(self,self._models[index])
+	
+	def add_model(self,model):
+		self._models.append(model)
+		self.store_changes()
+
+	def model_names(self):
+		return [ x._name for x in self._models]
+	
+	def models_to_keep(self,modelNameList):
+		returnValue= False
+		d = { x._name:x for x in self._models}
+		self._models = []
+		for n in modelNameList:
+			self._models.append(d[n])
+		if len(self._models) < len(d):
+			returnValue = True
+			#the selected item was probably deleted
+			self.store_changes()
+		return returnValue
+
+
+	def store_changes(self):
+		f = open(self._fileName,"w")
+		#version number
+		jd = {}
+		types =[]
+		for t in self._types:
+			type = { "name":t._name, "category":t._category, "boxes":[(b[0],b[1]) for b in t._specialBoxes.keys()]}
+			types.append(type)
+		jd["Types"]=types
+
+		models =[]
+		for m in self._models:
+			model = {"name":m._name, "type":m._type.name, "hits": [(h[0],h[1]) for h in m._hitBoxes.keys()]}
+			models.append(model)
+		jd["Models"] = models
+		f.write(json.dumps(jd))
+
+	def retrieve_changes(self):
+		try:
+			f = open(self._fileName,'r')
+			self._types =[]
+			self._models = []
+			js = json.loads(f.readline())
+
+			nameToTypeIndex ={}
+			for t in js['Types']:
+				name = t['name']
+				category = t['category']
+				boxes = t['boxes']
+				self._types.append(ModelType(name,category, { (b[0],b[1]):HitBoxQualities() for b in boxes}))
+				nameToTypeIndex[name]=len(self._types)-1
+			for m in js['Models']:
+				name = m['name']
+				type = m['type']
+				hits = m['hits']
+				self._models.append(Model(self.type(nameToTypeIndex[type]),name,{ (h[0],h[1]):True for h in hits}))
+		except IOError:
+			pass
+
+gDataSource = DataSource()
+
+
+#############################
+# Build Interface
+#############################
 
 def recursive_disabled(view,disable):
 	view.hidden = disable
-		
-def remove_all_models_of_type(type):
-	global gKnownModels
-	gKnownModels = [ x for x in gKnownModels if x._type != type]
-	
+
 def type_edit_action(sender):
-	global gKnownTypes
+	global gDataSource
 	global runningView
 	global list_last_selected_row
-	global listView
-	d = { x._name:x for x in gKnownTypes}
-	gKnownTypes = []
-	newNames = set()
-	for n in sender.items:
-		gKnownTypes.append(d[n])
-		newNames.add(n)
-	if len(gKnownTypes) < len(d):
+	if gDataSource.types_to_keep(sender.items):
 		#the selected item was probably deleted
 		recursive_disabled(runningView['main'],True)
-		for n in d.keys():
-			if n not in newNames:
-				remove_all_models_of_type(d[n])
-		#TODO: Should remove any model's using the removed Type
-	store_changes()
 	list_last_selected_row = -1
 
 def update_type_list_ui():
 	global listView
-	listView.data_source.items = [ x._name for x in gKnownTypes]
+	listView.data_source.items = gDataSource.type_names()
 	listView.reload()
 
 list_last_selected_row = -1
@@ -63,16 +214,17 @@ def type_chosen(sender):
 	previousChoice = nameView.text
 	newSelection = sender.selected_row
 	if list_last_selected_row != -1:
-		if listView.data_source.items[list_last_selected_row] != previousChoice:
+		t =gDataSource.type(list_last_selected_row)
+		if t.name !=previousChoice:
+			t.name = previousChoice
 			listView.data_source.items[list_last_selected_row] = previousChoice
-			gKnownTypes[list_last_selected_row]._name = previousChoice
 			listView.reload()
 	list_last_selected_row = newSelection
 	if -1 == newSelection:
 		recursive_disabled(runningView['main'],True)
 	else:
 		recursive_disabled(runningView['main'],False)
-		nameView.text = gKnownTypes[newSelection]._name
+		nameView.text = gDataSource.type(newSelection).name
 		setup_build_hit()
 				
 	#runningView['main'].set_needs_display()
@@ -81,9 +233,8 @@ def change_type_name(sender):
 	global listView
 	index = listView.data_source.selected_row
 	if -1 != index:
-		gKnownTypes[index]._name = sender.text
+		gDataSource.type(index).name = sender.text
 	update_type_list_ui()
-	store_changes()
 
 		
 
@@ -104,10 +255,10 @@ def setup_build_hit():
 		index = listView.data_source.selected_row
 		hitsView = runningView['main']['Hit Builder']
 		if index != -1:
-			qualities = gKnownTypes[index]._specialBoxes
+			t = gDataSource.type(index)
 			for s in hitsView.subviews[0].subviews:
 				if hasattr(s,"column"):
-					if (s.column,s.position) in qualities:
+					if t.has_box(s.column,s.position):
 						build_set_hit_disabled(s)
 					else:
 						build_set_hit_enabled(s)
@@ -118,54 +269,40 @@ def setup_build_hit():
 		hitsView.set_needs_display()
 			
 def build_hit_pressed(sender):
-	global gKnownTypes
+	global gDataSource
 	index = listView.data_source.selected_row	
 	if index == -1:
 		return
 	if sender.hit:
 		build_set_hit_enabled(sender)
-		key = (sender.column,sender.position)
-		if key in gKnownTypes[index]._specialBoxes:
-			del gKnownTypes[index]._specialBoxes[key]
+		gDataSource.type(index).remove_box(sender.column,sender.position)
 	else:
 		build_set_hit_disabled(sender)
-		#disable it	
-		#disable it	
-		gKnownTypes[index]._specialBoxes[(sender.column,sender.position)]=HitBoxQualities(active=False)
-	store_changes()
+		gDataSource.type(index).add_box(sender.column,sender.position)
+
+def add_type(sender):
+	global gDataSource
+	gDataSource.add_type(ModelType("New"))
+	update_type_list_ui()
+	#note I can not get the new row selected
+
 
 #############################
-# Playing
+# Play Interface
 #############################
-class Model (object):
-	def __init__(self,type,name,hits=None):
-		self._name = name
-		self._type = type
-		if hits:
-			self._hitBoxes =hits
-		else:
-			self._hitBoxes = {}
-
-gKnownModels =[]
 
 def model_edit_action(sender):
-	global gKnownModels
+	global gDataSource
 	global runningView
 	global list_last_selected_row
-	global listView
-	d = { x._name:x for x in gKnownModels}
-	gKnownModels = []
-	for n in sender.items:
-		gKnownModels.append(d[n])
-	if len(gKnownModels) < len(d):
+	if gDataSource.models_to_keep(sender.items):
 		#the selected item was probably deleted
 		recursive_disabled(runningView['main'],True)
-	store_changes()
 	list_last_selected_row = -1
 
 def update_model_list_ui():
 	global listView
-	listView.data_source.items = [ x._name for x in gKnownModels]
+	listView.data_source.items = gDataSource.model_names()
 	listView.reload()
 
 def model_chosen(sender):
@@ -178,19 +315,20 @@ def model_chosen(sender):
 		#might have selected a new row without first hitting return in the name text field
 		if listView.data_source.items[list_last_selected_row] != previousChoice:
 			listView.data_source.items[list_last_selected_row] = previousChoice
-			gKnownModels[list_last_selected_row]._name=previousChoice
+			gDataSource.model(list_last_selected_row).name=previousChoice
 			listView.reload()
 	list_last_selected_row = newSelection
 	if -1 == newSelection:
 		recursive_disabled(runningView['main'],True)
 	else:
 		recursive_disabled(runningView['main'],False)
-		nameView.text = gKnownModels[newSelection]._name
-		runningView['main']['type chooser'].title = gKnownModels[newSelection]._type._name
+		m =gDataSource.model(newSelection)
+		nameView.text = m.name
+		runningView['main']['type chooser'].title = m.type.name
 		setup_model_hit()		
 
 def make_unique_model_name(name):
-	while name in [x._name for x in gKnownModels]:
+	while name in gDataSource.model_names():
 		name += ' 1'
 	return name
 
@@ -200,14 +338,14 @@ def change_model_name(sender):
 	if -1 != index:
 		#name must be unique
 		name = sender.text
-		if name == gKnownModels[index]._name:
+		model =gDataSource.model(index)
+		if name == model.name:
 			return
 		name = make_unique_model_name(name)
-		gKnownModels[index]._name = name
+		model.name = name
 		if name != sender.text:
 			sender.text = name
 	update_model_list_ui()
-	store_changes()
 
 def play_set_hit_enabled(hit):
 	hit.hidden =False
@@ -231,17 +369,16 @@ def play_set_unhit(hit):
 
 def setup_model_hit():
 	index = listView.data_source.selected_row
-	model = gKnownModels[index]
-	qualities = model._type._specialBoxes
-	hits = model._hitBoxes
+	model = gDataSource.model(index)
+	mType = model.type
 	hitsView = runningView['main']['Hit Filler']
 	for s in hitsView.subviews[0].subviews:
 		if hasattr(s,"column"):
-			if (s.column,s.position) in qualities:
+			if mType.has_box(s.column,s.position):
 				play_set_hit_disabled(s)
 			else:
 				play_set_hit_enabled(s)
-			if (s.column,s.position) in hits:
+			if model.was_hit(s.column,s.position):
 				play_set_hit(s)
 			else:
 				play_set_unhit(s)
@@ -251,15 +388,12 @@ def play_hit_pressed(sender):
 	if sender.hit:
 		sender.hit = False
 		sender.image = notHitImage
-		key = (sender.column,sender.position)
-		if key in gKnownModels[index]._hitBoxes:
-			del gKnownModels[index]._hitBoxes[key]
+		gDataSource.model(index).remove_hit(sender.column,sender.position)
 	else:
 		sender.hit = True
 		sender.image = hitImage
-		gKnownModels[index]._hitBoxes[(sender.column,sender.position)] = True
-	store_changes()
-		
+		gDataSource.model(index).add_hit(sender.column,sender.position)
+
 hit_pressed = None
 
 buildButton = None
@@ -331,16 +465,10 @@ def play_pressed(sender):
 	hitView.width  = mainView.width
 	runningView['main']['Hit Filler'].add_subview(hitView)
 
-def add_type(sender):
-	global gKnownTypes
-	gKnownTypes.append(ModelType("New"))
-	update_type_list_ui()
-	#note I can not get the new row selected
-		
 def add_model(sender):
-	global gKnownModels
-	t = gKnownTypes[0]
-	gKnownModels.append(Model(t,t._name+" "+str(len(gKnownModels))))
+	global gDataSource
+	t = gDataSource.type(0)
+	gDataSource.add_model(Model(t,t.name+" "+str(len(gDataSource.model_names()))))
 	update_model_list_ui()
 	pass
 	
@@ -357,7 +485,7 @@ def choose_type(sender):
 	v = ui.TableView()
 	v.allows_selection=True
 	v.set_editing=False
-	ls = ui.ListDataSource([t._name for t in gKnownTypes])
+	ls = ui.ListDataSource(gDataSource.type_names())
 	v.data_source= ls
 	v.delegate = ls
 	cd = ChoiceDelegate(v)
@@ -366,53 +494,12 @@ def choose_type(sender):
 	v.present('sheet')
 	v.wait_modal()
 	if cd._choice != -1:
-		sender.title = gKnownTypes[cd._choice]._name
-		gKnownModels[listView.data_source.selected_row]._type = gKnownTypes[cd._choice]
-		store_changes()
+		t =gDataSource.type(cd._choice)
+		sender.title = t.name
+		gDataSource.model(listView.data_source.selected_row).type = t
 		setup_model_hit()
 
-def store_changes():
-	f = open(".cid","w")
-	#version number
-	jd = {}
-	types =[]
-	for t in gKnownTypes:
-		type = { "name":t._name, "category":t._category, "boxes":[(b[0],b[1]) for b in t._specialBoxes.keys()]}
-		types.append(type)
-	jd["Types"]=types
-	
-	models =[]
-	for m in gKnownModels:
-		model = {"name":m._name, "type":m._type._name, "hits": [(h[0],h[1]) for h in m._hitBoxes.keys()]}
-		models.append(model)		
-	jd["Models"] = models
-	f.write(json.dumps(jd))
-
-def retrieve_changes():
-	global gKnownTypes
-	global gKnownModels
-	try:
-		f = open('.cid','r')
-		gKnownTypes =[]
-		gKnownModels = []
-		js = json.loads(f.readline())
-
-		nameToType ={}
-		for t in js['Types']:
-			name = t['name']
-			category = t['category']
-			boxes = t['boxes']
-			gKnownTypes.append(ModelType(name,category, { (b[0],b[1]):HitBoxQualities() for b in boxes}))
-			nameToType[name]=gKnownTypes[-1]
-		for m in js['Models']:
-			name = m['name']
-			type = m['type']
-			hits = m['hits']
-			gKnownModels.append(Model(nameToType[type],name,{ (h[0],h[1]):True for h in hits}))
-	except IOError:
-		pass
-
-retrieve_changes()
+gDataSource.retrieve_changes()
 v = ui.load_view()
 mainView=v['Main']
 buildButton=v['bottom']['mode buttons']['Build']
